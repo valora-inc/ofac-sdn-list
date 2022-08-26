48363ffd2c2b6b8102a86e4c00d1f3aaa352945a
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-08-25 19:23:05 -0700

    Initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: ['@valora/eslint-config-typescript'],
   parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  rules: {
-    // Maybe move it to @valora/eslint-config-typescript?
-    'jest/valid-title': ['error', { ignoreTypeOfDescribeName: true }],
+    project: './tsconfig.json',
   },
   ignorePatterns: ['tsconfig.json'],
 }
--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { main } from 'src'
-
-describe(main, () => {
-  it("should return 'Hello, world!'", () => {
-    expect(main()).toBe('Hello, world!')
-  })
-})
